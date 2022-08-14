0fcc46cffa68abfc33f4f524658d14f600a2e59d
Gurjot singh <78019426+Gur-jot@users.noreply.github.com>
2022-08-14 21:13:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
 /*
@@ -8,7 +6,6 @@ particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
 */
-
 /* Otherwise just put the config content (json): */
 
 particlesJS('particles-js',
